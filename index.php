594813703d3c0e71ce77efc1340504a524b7f7ee
danimask1806 <mr.danielsi18@gmail.com>
2018-11-16 18:42:19 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <!DOCTYPE html>
 <html>
     <body>
+             <form method="post">
+                  <input id="nombre" name="nombre" type="text" >
+				  <input id="fecha"  name="fecha" type="text" >
+                  <input id="hora" name="hora" type="text">
+                  <input id="estado" name="estado" type="text" >
+                  <input id="prioridad" name="prioridad" type="text" >
+                  <button id="btnvalidar" type="submit" name="validar tareas" > </button>
+                 <button id="btninsertar" type="submit" name="validar tareas" > </button>
+                 <button id="btnborrar" type="submit" name="validar tareas" > </button>
+                 
+                 
+            </form>
     
       <?php
 $servername = "db4free.net";
@@ -8,6 +20,30 @@ $username = "tareasroot";
 $password = "password";
 $db_name = "tareasdb1234";
 
+        if (isset($_POST['btnvalidar'])) {
+    $id = "";
+    $nombre = $_POST['nombre'];
+    $fecha = $_POST['fecha'];
+    $hora = $_POST['hora'];
+    $estado = $_POST['estado'];
+    $prioridad = $_POST['prioridad'];
+            
+     $sql = "SELECT id, nombre, fecha, hora, estado,prioridad FROM horario";
+    $result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["dia"]. " - Name: " . $row["nombre"]. " " . $row["hora"]. "<br>";
+        $en_csv .= "dia: " . $row["dia"]. " - Name: " . $row["nombre"]. " " . $row["hora"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+    print $en_csv;
+            
+        }
+        
 // Create connection
 $conn = new mysqli($servername, $username, $password,$db_name);
 
@@ -30,19 +66,7 @@ if ($conn->query($peticion) === TRUE) {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-$sql = "SELECT id, firstname, lastname FROM horario";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-        $en_csv .= "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-    print $en_csv;
 
     $sql = "DELETE FROM horario WHERE id=2";
 
