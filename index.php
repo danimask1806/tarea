4ec82c529b15d64675a209f02187b1b58273e1d0
danimask1806 <mr.danielsi18@gmail.com>
2018-11-16 01:51:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 <html>
     <body>
     
-        <? php 
-$enlace = mysqli_connect("localhost" , "id7866198_root" , "password" , "id7866198_tareasdb");
-if (!$enlace) {
-    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
+        <?php 
+        $enlace = mysqli_connect("localhost" , "id7866198_root" , "password" , "id7866198_tareasdb");
+        if (!$enlace) {
+            echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+            echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+            echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+            exit;
+        }
 
 echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
 echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
