a013a8ea5e9cc7f7336bb0a0f6dad17de30d1c8f
danimask1806 <mr.danielsi18@gmail.com>
2018-11-16 12:37:03 -0500

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 <html>
     <body>
     
-        <?php 
-        
-        $conn = mysqli_connect(“mysql#.000webhost.com”, “a3367xxx_test”,"******", “a3367xxx_test”) or die(mysql_error());
-        }
+      <?php
+$servername = "databases.000webhost.com";
+$username = "id7866198_root";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 ?>
 
 
