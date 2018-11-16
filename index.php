473ee4346afdf24d3e7538f1b8f4b3a8daa04a16
danimask1806 <mr.danielsi18@gmail.com>
2018-11-16 13:12:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $username = "tareasroot";
 $password = "password";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password,"tareasdb1234");
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
