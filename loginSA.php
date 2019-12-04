054c17d6697caa0f49d224529bbcbecee0b32545
danimask1806 <mr.danielsi18@gmail.com>
2019-12-04 14:39:04 -0500

    Update loginSA.php

--- loginSA.php
+++ loginSA.php
@@ -1,55 +1,56 @@
-<?php 
-	// declaración de variable
-	$ username  =  " " ;
-	$ email     =  " " ;
-	$ errores  =  array ();
-	// conectarse a la base de datos
-	$ db  =  mysqli_connect ( ' db4free.net ' , ' tareasroot ' , ' contraseña ' , ' tareasdb1234 ' );
-	// REGISTRO DE USUARIO
-	if ( isset ( $ _POST [ ' reg_user ' ])) {
-		// recibir todos los valores de entrada del formulario
-		$ username  =  mysqli_real_escape_string ( $ db , $ _POST [ ' nombre de usuario ' ]);
-		$ email  =  mysqli_real_escape_string ( $ db , $ _POST [ ' correo electrónico ' ]);
-		$ contraseña_1  =  mysqli_real_escape_string ( $ db , $ _POST [ ' contraseña_1 ' ]);
-		$ contraseña_2  =  mysqli_real_escape_string ( $ db , $ _POST [ ' contraseña_2 ' ]);
-		// validación del formulario: asegúrese de que el formulario se complete correctamente
-		if ( empty ( $ username )) { array_push ( $ errors , " Se requiere nombre de usuario " ); }
-		if ( vacío ( $ correo electrónico )) { array_push ( $ errores , " Se requiere correo electrónico " ); }
-		if ( vacío ( $ contraseña_1 )) { array_push ( $ errores , " Se requiere contraseña " ); }
-		if ( $ contraseña_1  ! =  $ contraseña_2 ) {
-			array_push ( $ errores , " Las dos contraseñas no coinciden " );
+<?php
+
+	// variable declaration
+	$username = "";
+	$email    = "";
+	$errors = array(); 
+	// connect to database
+	$db = mysqli_connect('db4free.net', 'tareasroot', 'password', 'tareasdb1234');
+	// REGISTER USER
+	if (isset($_POST['reg_user'])) {
+		// receive all input values from the form
+		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$email = mysqli_real_escape_string($db, $_POST['email']);
+		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+		// form validation: ensure that the form is correctly filled
+		if (empty($username)) { array_push($errors, "Username is required"); }
+		if (empty($email)) { array_push($errors, "Email is required"); }
+		if (empty($password_1)) { array_push($errors, "Password is required"); }
+		if ($password_1 != $password_2) {
+			array_push($errors, "The two passwords do not match");
 		}
-		// registrar usuario si no hay errores en el formulario
-		if ( cuenta ( $ errores ) ==  0 ) {
-			$ query  =  " INSERTAR EN usuarios (nombre de usuario, correo electrónico, contraseña)
-					  VALUES ( ' $ nombre de usuario ' , ' $ correo electrónico ' , ' $ contraseña_1 ' ) " ;
-			if ( $ db -> query ( $ query ) ===  TRUE ) {
-			    echo  " Nueva tabla bien creada " ;
-			} más {
-			    echo  " Error: "  .  $ consulta  .  " <br> "  .  $ db -> error ;
+		// register user if there are no errors in the form
+		if (count($errors) == 0) {
+			$query = "INSERT INTO users (username, email, password) 
+					  VALUES('$username', '$email', '$password_1')";
+			if ($db->query($query) === TRUE) {
+			    echo "Nueva tabla bien creada";
+			} else {
+			    echo "Error: " . $query . "<br>" . $db->error;
 			}
 			
 		}
 	}
-	// ...
-	// INICIAR SESIÓN
-	if ( isset ( $ _POST [ ' login_user ' ])) {
-		$ username  =  mysqli_real_escape_string ( $ db , $ _POST [ ' nombre de usuario ' ]);
-		$ contraseña  =  mysqli_real_escape_string ( $ db , $ _POST [ ' contraseña ' ]);
-		if ( vacío ( $ nombre de usuario )) {
-			array_push ( $ errores , " Se requiere nombre de usuario " );
+	// ... 
+	// LOGIN USER
+	if (isset($_POST['login_user'])) {
+		$username = mysqli_real_escape_string($db, $_POST['username']);
+		$password = mysqli_real_escape_string($db, $_POST['password']);
+		if (empty($username)) {
+			array_push($errors, "Username is required");
 		}
-		if ( vacío ( $ contraseña )) {
-			array_push ( $ errores , " Se requiere contraseña " );
+		if (empty($password)) {
+			array_push($errors, "Password is required");
 		}
-		if ( cuenta ( $ errores ) ==  0 ) {
-			$ query  =  " SELECT  *  FROM users WHERE username = ' $ username '  AND password = ' $ password ' " ;
-			$ resultados  =  mysqli_query ( $ db , $ consulta );
-			if (mysqli_num_rows ( $ resultados ) ==  1 ) {
-				echo "Login sucess";
-			} más {
-				array_push ( $ errores , " combinación de nombre de usuario / contraseña incorrecta " );
+		if (count($errors) == 0) {
+			$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+			$results = mysqli_query($db, $query);
+			if (mysqli_num_rows($results) == 1) {
+				echo "login sucess ";
+			}else {
+				array_push($errors, "Wrong username/password combination");
 			}
 		}
 	}
-? >
+?>
