0cd54bd26d285ea5eb8e08c2303cfae8cbed9d84
danimask1806 <mr.danielsi18@gmail.com>
2019-12-04 14:06:59 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,3 +1,36 @@
-<? php  
-echo  " hola mama " ;
-? >
+<? php include ( ' server.php ' ) ? > 
+<! DOCTYPE html>
+< html >
+< cabeza >
+	< título > MA </ título >
+	< link  rel = " stylesheet "  type = " text / css "  href = " style.css " >
+</ cabeza >
+< body  background = " 123456789.svg " >
+< cuerpo >
+	< div  class = " header " >
+		< h2 > Iniciar sesión </ h2 >
+	</ div >
+	
+	< form  method = " post "  action = " login.php " >
+
+		<? php include ( ' errors.php ' ); ? > 
+
+		< div  class = " input-group " >
+			< etiqueta > Nombre de usuario </ etiqueta >
+			< input  type = " text "  name = " username " >
+		</ div >
+		< div  class = " input-group " >
+			< etiqueta > Contraseña </ etiqueta >
+			< input  type = " password "  name = " password " >
+		</ div >
+		< div  class = " input-group " >
+			< input  type = " submit "  name = " login_user " > Iniciar sesión </ button >
+		</ div >
+		< p >
+			¿Todavía no eres miembro? < Un  href = " loginSA.php " > Regístrese </ a >
+		</ p >
+	</ form >
+
+
+</ cuerpo >
+</ html >
