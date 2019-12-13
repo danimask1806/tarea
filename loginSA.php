985e8459a43d0c3be0c64fe8bdea68287eb35951
danimask1806 <mr.danielsi18@gmail.com>
2019-12-13 15:01:32 -0500

    Update loginSA.php

--- loginSA.php
+++ loginSA.php
@@ -2,12 +2,9 @@
 
 	// variable declaration
 
-	$_POST["login_user"] = "1";
 	$username = "";
 	$email    = "";
 	$errors = array();
-	$login_user = $_POST["login_user"]; 
-	$reg_user = $_POST['reg_user']; 
 	// connect to database
 	$db = mysqli_connect('db4free.net', 'tareasroot', 'password', 'ultimatumgamesdb');
 	if (mysqli_connect_errno()) {
@@ -15,25 +12,21 @@
     		exit();
 	}
 	// REGISTER USER
-	if ($reg_user == "1") {
+	if (isset($_POST['register'])) {
 		// receive all input values from the form
 		$username = mysqli_real_escape_string($db, $_POST['username']);
 		$email = mysqli_real_escape_string($db, $_POST['email']);
-		$password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
-		$password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+		$password_1 = mysqli_real_escape_string($db, $_POST['password']);
 		// form validation: ensure that the form is correctly filled
 		if (empty($username)) { array_push($errors, "Username is required"); }
 		if (empty($email)) { array_push($errors, "Email is required"); }
 		if (empty($password_1)) { array_push($errors, "Password is required"); }
-		if ($password_1 != $password_2) {
-			array_push($errors, "The two passwords do not match");
-		}
 		// register user if there are no errors in the form
 		if (count($errors) == 0) {
 			$query = "INSERT INTO users (username, email, password) 
 					  VALUES('$username', '$email', '$password_1')";
 			if ($db->query($query) === TRUE) {
-			    echo "Nueva tabla bien creada";
+			    echo "new user";
 			} else {
 			    echo "Error: " . $query . "<br>" . $db->error;
 			}
@@ -60,7 +53,6 @@
 			echo " no errors";
 			$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
 			$results = mysqli_query($db, $query);
-			echo $username.$password;
 			if (mysqli_num_rows($results) == 1) {
 				echo "login sucess ";
 			}else {
